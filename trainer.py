e710c6e942cf53ef992018d0cf22509e03b99342
PromotezCitizen <gkstkdgus821@gmail.com>
2025-08-30 17:43:14 +0900

    훈련 메인 로직 수정

--- trainer.py
+++ trainer.py
@@ -1,4 +1,4 @@
-# import os
+import os
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -17,7 +17,7 @@ import torch
 
 from helpers.s3_helper import S3Helper
 
-project_root_path = Path(__file__).parent
+project_root_path = Path(__file__).parent.resolve()
 
 def train_and_eval(model:TrainModel, loop:int) -> None:
     print(f' {model.model_type}')
@@ -45,11 +45,15 @@ if __name__ == "__main__":
     train_group.add_argument('-tn', '--train-nickname', action='store_true', help='닉네임 모델 훈련을 진행합니다.')
 
     utility_group = parser.add_argument_group('Utility Options')
-    utility_group.add_argument('-u', '--upload', action='store_true', help='생성한 모델들을 업로드합니다.')
     utility_group.add_argument('-ra', '--reset-all', action='store_true', help='모든 모델을 삭제합니다.')
     utility_group.add_argument('-rt', '--reset-tokenizer', action='store_true', help='토크나이저를 삭제합니다.')
     utility_group.add_argument('-rc', '--reset-comment', action='store_true', help='댓글 모델을 삭제합니다.')
     utility_group.add_argument('-rn', '--reset-nickname', action='store_true', help='닉네임 모델을 삭제합니다.')
+
+    utility_group.add_argument('-ua', '--upload-all', action='store_true', help='생성한 모델들을 업로드합니다.')
+    utility_group.add_argument('-ut', '--upload-tokenizer', action='store_true', help='생성한 토크나이저를 업로드합니다.')
+    utility_group.add_argument('-uc', '--upload-comment', action='store_true', help='생성한 댓글 모델을 업로드합니다.')
+    utility_group.add_argument('-un', '--upload-nickname', action='store_true', help='생성한 닉네임 모델을 업로드합니다.')
     
     args = parser.parse_args()
 
@@ -75,15 +79,20 @@ if __name__ == "__main__":
         args.reset_nickname = True
 
     if args.train_all:
+        args.train_tokenizer = True
         args.train_comment = True
         args.train_nickname = True
-        args.train_tokenizer = True
 
-    if args.upload or args.train_comment or args.train_nickname:
+    if args.upload_all:
+        args.upload_tokenizer = True
+        args.upload_comment = True
+        args.upload_nickname = True
+
+    if args.upload_tokenizer or args.upload_comment or args.upload_nickname or args.train_comment or args.train_nickname:
         helper = S3Helper(str(project_root_path), 'youtube-comment-predict')
 
-    nickname_train_loops, nickname_train_epoches = 1, 5
-    comment_train_loops, comment_train_epoches = 1, 5
+    nickname_train_loops, nickname_train_epoches = 1, 3
+    comment_train_loops, comment_train_epoches = 1, 3
 
     if args.reset_tokenizer:
         remove_model('tokenizer')
@@ -141,6 +150,15 @@ if __name__ == "__main__":
         comment_model.save()
         del comment_model
 
-    if args.upload:
-        helper.upload(from_local=True)
+
+    if args.upload_tokenizer or args.upload_nickname or args.upload_comment:
+        target_upload_path = []
+        base_model_path = os.path.join(project_root_path, 'model')
+        if args.upload_tokenizer:
+            target_upload_path.append(os.path.join(base_model_path, 'tokenizer'))
+        if args.upload_nickname:
+            target_upload_path.append(os.path.join(base_model_path, 'nickname_model_fp16'))
+        if args.upload_comment:
+            target_upload_path.append(os.path.join(base_model_path, 'comment_model_fp16'))
+        helper.upload(local_fpaths=target_upload_path)
         print('upload finished')

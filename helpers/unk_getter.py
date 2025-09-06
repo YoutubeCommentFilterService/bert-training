029e9bc3f85d0e802a4bdfee6b1a420719a70b53
PromotezCitizen <gkstkdgus821@gmail.com>
2025-09-06 21:20:01 +0900

    닉네임 unk 토큰 추출 로직 추가

--- helpers/unk_getter.py
+++ helpers/unk_getter.py
@@ -3,7 +3,7 @@ import pandas as pd
 from text_preprocessing import TextNormalizator
 import re
 import os
-
+import time
 
 tokenizer = AutoTokenizer.from_pretrained('../model/tokenizer')
 UNKNOWN_TOKEN = tokenizer.unk_token
@@ -18,18 +18,25 @@ origin_datas = datas.copy(True)
 # datas = pd.read_csv('./testset.csv')
 
 normalizator = TextNormalizator()
+print('preprocessing started')
+start = time.time()
 normalizator.run_text_preprocessing(datas)
+print('preprocessing ended - ', time.time() - start)
 
 os.makedirs('./unk_data', exist_ok=True)
 
-
-with open('./unk_data/datas', 'w', encoding='utf-8') as f:
-    f.write('')
-with open('./unk_data/unk_datas_char', 'w', encoding='utf-8') as f:
-    f.write('')
+UNK_COMMENT_FILE = './unk_data/datas.txt'
+UNK_COMMENT_CHAR_FILE = './unk_data/unk_datas_char.txt'
+UNK_NICK_FILE = './unk_data/datas_nick.txt'
 
 datas['comment'] = datas['comment'].str.strip()
 
+print('comment unk getter started')
+with open(UNK_COMMENT_FILE, 'w', encoding='utf-8') as f:
+    f.write('')
+with open(UNK_COMMENT_CHAR_FILE, 'w', encoding='utf-8') as f:
+    f.write('')
+start = time.time()
 for idx, data in enumerate(datas['comment']):
     tokens = tokenizer.tokenize(data)
     write_text = data if origin_datas["comment"].iloc[idx] == data else f'{origin_datas["comment"].iloc[idx]}\n{data}'
@@ -47,8 +54,24 @@ for idx, data in enumerate(datas['comment']):
         processing_text = processing_text.replace(current_word, '', 1)
         processing_text = re.sub(r'\s+', ' ', processing_text).strip()
 
-        with open('./unk_data/unk_datas_char', 'a+', encoding='utf-8') as f:
+        with open(UNK_COMMENT_CHAR_FILE, 'a+', encoding='utf-8') as f:
             f.write(f'{write_text}\n\t{processing_text}\n\n')
 
-    with open('./unk_data/datas', 'a+', encoding='utf-8') as f:
+    with open(UNK_COMMENT_FILE, 'a+', encoding='utf-8') as f:
         f.write(f'{write_text}\n\t{tokens}\n')
+print('comment unk getter ended - ', time.time() - start)
+
+print('nickname unk getter started')
+with open(UNK_NICK_FILE, 'w', encoding='utf-8') as f:
+    f.write('')
+origin_nickname = origin_datas['nickname'].dropna().reset_index(drop=True)
+start = time.time()
+for idx, data in enumerate(datas['nickname'].dropna().reset_index(drop=True)):
+    try:
+        tokens = tokenizer.tokenize(data)
+        write_text = data if origin_nickname.iloc[idx] == data else f'{origin_nickname.iloc[idx]}\n{data}'
+        with open(UNK_NICK_FILE, 'a+', encoding='utf-8') as f:
+            f.write(f'{write_text}\n\t{tokens}\n')
+    except:
+        print(idx, data)
+print('nickname unk getter ended - ', time.time() - start)
